57cd02485bd6d3297415da2d13c98a7830eb864b
SeungHyun <fm10033@gmail.com>
2021-07-29 16:08:20 +0900

    app.js reducer로 전환

--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,35 @@
-import React, {useState} from 'react'
+import React, { useReducer } from 'react'
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        default:
+            throw new Error('unhandeled action')
+    }
+}
 
 function Counter() {
-    const [number, setNumber] = useState(0)
+
+    const [number, dispatch] = useReducer(reducer, 0)
+
     const increase = () => {
-        setNumber(number + 1)
+        dispatch({
+            type:'INCREMENT'
+        })
     }
     const decrease = () => {
-        setNumber(number -  1)
+        dispatch({
+            type:'DECREMENT'
+        })
     }
     return (
         <div>
             <h1>{number}</h1>
-            <button onClick = {increase}>+1</button>
-            <button onClick = {decrease}>-1</button>
+            <button onClick={increase}>+1</button>
+            <button onClick={decrease}>-1</button>
         </div>
     )
 
